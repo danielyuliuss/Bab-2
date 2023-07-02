efa99b0178346e51221451c60ee773c1f4a5014d
Daniel Yulius S <danielyuliussoetjipto@gmail.com>
2023-07-03 04:12:09 +0700

    Add project files.

---
+++ Bab 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string nama = "Daniel";
+            Console.WriteLine(nama);
+
+            int x = 10;
+            int y = 5;
+            if(x<y)
+            {
+                Console.WriteLine("Nilai X kurang dari Y");
+            }
+            else
+            {
+                Console.WriteLine("Nilai X lebih dari Y");
+            }
+        }
+    }
+}
